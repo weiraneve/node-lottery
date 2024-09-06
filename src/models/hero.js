0141e1fc0f5c9a5aeaff6b4772413901ff2e964c
weiraneve <907221539@qq.com>
2024-09-06 16:15:09 +0800

    feat: add hero model

---
+++ src/models/hero.js
@@ -0,0 +1,10 @@
+class Hero {
+    constructor(id, name, line, isPick) {
+        this.id = id;
+        this.name = name;
+        this.line = line;
+        this.isPick = isPick;
+    }
+}
+
+module.exports = Hero;  
